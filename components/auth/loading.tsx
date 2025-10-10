66104b2922fd897fa405c2129615d63ce590afcd
Mohaib Molla <mohaibmolla1@gmail.com>
2025-10-10 10:58:29 +0530

    damm

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -17,6 +17,6 @@ export const Loading = () => {
 
 
 
-//hey hey
+
 
 
--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,11 +1,11 @@
+
 export default {
     providers: [
         {
-            // Replace with your own Clerk Issuer URL from your "convex" JWT template
-            // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
-            // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
-            // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-            domain: "https://clerk.mohaib.me",
+            // This should be your Clerk JWT Issuer URL
+            // Format: https://<clerk-subdomain>.clerk.accounts.dev
+            // NOT your custom domain
+            domain: "https://proud-albacore-77.clerk.accounts.dev",
             applicationID: "convex",
         },
     ]
